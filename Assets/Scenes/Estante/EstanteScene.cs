c17cae6c2dc0669d9f36bb27d1d9c94c052e1393
samuel.matos <samuel.matos@wildlifestudios.com>
2025-04-03 09:42:53 -0300

    feat: asking for notifications policy access on estante scene

--- Assets/Scenes/Estante/EstanteScene.cs
+++ Assets/Scenes/Estante/EstanteScene.cs
@@ -2,9 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Notifications;
 
 public class EstanteScene : MonoBehaviour
 {
+    public ISupressNotifications notifications;
+
+    public void Start()
+    {
+        notifications = new SupressNotifications();
+
+        if (!notifications.IsNotificationPolicyAccessGranted())
+        {
+            notifications.AskForNotificationPolicyAccess();
+        }
+    }
+
     public void OnTimerlicked() => SceneManager.LoadScene("ConfigTimerScene");
     public void OnSettingsClicked() => SceneManager.LoadScene("Configuracoes Scene");
 }
